fb0d1f0b7742612936668d7e56cb8cd64ab73b52
zzayac5 <z.zayac@winthewarcs.com>
2026-01-19 10:30:17 -0500

    added time to game state

--- main.py
+++ main.py
@@ -5,12 +5,18 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
-        for event in pygame.event.get():
-            pass
         screen.fill("black")
         pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        dt = clock.tick(60.0) / 1000
+
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
