6b0b85f94368e80a73cab34534355c0347663fe4
zzayac5 <z.zayac@winthewarcs.com>
2026-01-19 11:04:57 -0500

    added player object to screen

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_RADIUS
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         log_state()
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         dt = clock.tick(60.0) / 1000
+
 
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
